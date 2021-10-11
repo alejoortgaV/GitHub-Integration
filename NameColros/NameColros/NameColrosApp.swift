021bb2f8a3edf779eba36cfb78b398b8f8eb6564
Alejoortga <calejo.ortega@hotmail.com>
2021-10-10 22:15:50 -0500

    This is the first commit message.

---
+++ NameColros/NameColros/NameColrosApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameColrosApp.swift
+//  NameColros
+//
+//  Created by Cristian Alejandro Forero Ortega on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct NameColrosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
